900a1404fc7f9c1e00a07f544801bee99f6473ce
CXLTxFVKE <theodore.kloesel@gmail.com>
2017-11-03 13:10:04 -0700

    Light2dAssets

---
+++ copyof2d/Assets/GlobalControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalControl : MonoBehaviour {
+	
+	public int killcount = 0;
+	public int gold = 0;
+	private GameObject thePlayer; 
+	private PlayerMovement playerScript;
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = GameObject.Find("Player");
+		playerScript = thePlayer.GetComponent<PlayerMovement>();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		//var playerScript = thePlayer.GetComponent<PlayerMovement>();
+		if (playerScript.playerlevel == killcount)
+		killcount = 0;
+		playerScript.playerlevel++;
+	}
+}
---
+++ copyof2d/Assets/gunlightscript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunlightscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = Vector3.one * Random.Range (3.5f, 4f);
+		Destroy (gameObject, Random.Range(0.02f,0.06f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
